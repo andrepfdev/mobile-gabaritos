4bace2bfefc9c715833d3071e8654a43a47f8111
Rogerio Junior <rogeriojr1100@gmail.com>
2026-08-02 02:55:04 -0300

    fix: standardize ESLint configuration formatting
    - Updated quotes in eslint.config.js for consistency.
    - Reformatted ignore patterns to use double quotes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
-const expoConfig = require('eslint-config-expo/flat');
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = [
   ...expoConfig,
   {
     ignores: [
-      'node_modules/**',
-      '.expo/**',
-      'android/**',
-      'ios/**',
-      'dist/**',
-      'tools/**',
+      "node_modules/**",
+      ".expo/**",
+      "android/**",
+      "ios/**",
+      "dist/**",
+      "tools/**",
     ],
   },
 ];
-
